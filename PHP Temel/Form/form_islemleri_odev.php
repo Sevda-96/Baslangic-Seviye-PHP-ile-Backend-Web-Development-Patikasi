edf0196a28bc0c28d6b148933e2c696bb2363e90
sevda engn <svdngn@outlook.com>
2022-04-05 02:52:01 +0300

    form islemleri

---
+++ PHP Temel/Form/form_islemleri_odev.php
@@ -0,0 +1,28 @@
+<?php
+//ob_start();
+function control(){
+   $number=$_POST['number'];
+   if($number==""){
+    echo "<script type='text/javascript'>alert('lütfen alanı boş bırakmayınız');</script>";
+   }
+   else{ 
+       if(is_numeric($number)){
+            if($number % 3 ==0 ){
+                echo"<h3>bölünebilir</h3>";
+            }
+            else{
+            while($number % 3 !=0 ){
+                $number++;
+            }
+            echo "<h3>Girilen Sayı : " . $number . "</h3><h3>Girdiğiniz sayı 3 ile bölünemez :( Bölünebilecek ilk sayı :" . $number."</h3>";
+            }
+        }
+        else{
+            echo "<script type='text/javascript'>alert('Lütfen sayi giriniz');</script>";
+        }
+        
+   }
+   
+}
+control();
+?>
